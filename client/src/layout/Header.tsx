8d92443542c43537bc1e17ef67a3f33c8fd3fa1c
hayamaster <dnfxmfkdbzos@naver.com>
2025-01-14 16:02:10 -0500

    feat: setting svg & add logo

--- client/src/layout/Header.tsx
+++ client/src/layout/Header.tsx
@@ -1,9 +1,11 @@
 import { ReactNode } from "react";
+import { Logo } from "../assets/icons";
 
 const Header = ({ children }: { children: ReactNode }) => {
   return (
     <div className="h-full w-full overflow-x-hidden flex flex-col">
-      <header className="w-full h-24 flex justify-center items-center">
+      <header className="w-full h-24 flex justify-around items-center">
+        <Logo className="w-28 h-10" />
         header
       </header>
       {children}
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+  plugins: [react(), svgr()],
+});
