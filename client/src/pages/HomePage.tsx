488a7993159408c3e779a6356a4e1ff53c8858da
hayamaster <dnfxmfkdbzos@naver.com>
2025-01-15 13:11:15 -0500

    feat: create category & sort options ui

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, MouseEvent } from "react";
 import { useGetListItems } from "@/apis/hooks";
 import { Logo, SearchIcon } from "@/assets/icons";
+import { Button } from "@/components/ui/button";
 
 interface ListItem {
   _id: string;
@@ -12,6 +13,20 @@ const HomePage = () => {
   const { data } = useGetListItems();
 
   const [search, setSearch] = useState<string>("");
+  const [clickedCategory, setClickedCategory] = useState<string>("all");
+  const [clickedSort, setClickedSort] = useState<string>("newest");
+
+  const handleCategoryClick = (e: MouseEvent<HTMLButtonElement>) => {
+    const target = e.target as HTMLButtonElement;
+
+    setClickedCategory(target.id);
+  };
+
+  const handleSortClick = (e: MouseEvent<HTMLButtonElement>) => {
+    const target = e.target as HTMLButtonElement;
+
+    setClickedSort(target.id);
+  };
 
   useEffect(() => {
     console.log(data);
@@ -44,7 +59,115 @@ const HomePage = () => {
           </button>
         </div>
       </header>
-      <h1>Home?</h1>
+
+      <div className="w-full flex flex-col justify-center items-center text-3xl lg:text-4xl xl:text-5xl font-bold py-10">
+        <h1>Find out people's</h1>
+        <h1>favorite places on maps!</h1>
+      </div>
+
+      <div className="flex w-full py-4 items-center">
+        <div className="flex w-full gap-1 items-center justify-start">
+          <Button
+            onClick={handleCategoryClick}
+            id="all"
+            variant={`${clickedCategory === "all" ? "default" : "outline"}`}
+            className="rounded-full text-xs xl:text-sm font-normal px-3 py-0.5 h-6"
+          >
+            All
+          </Button>
+          <Button
+            onClick={handleCategoryClick}
+            id="eats-drinks"
+            variant={`${clickedCategory === "eats-drinks" ? "default" : "outline"}`}
+            className="rounded-full text-xs xl:text-sm font-normal px-3 py-0.5 h-6"
+          >
+            Eats & Drinks
+          </Button>
+          <Button
+            onClick={handleCategoryClick}
+            id="dates"
+            variant={`${clickedCategory === "dates" ? "default" : "outline"}`}
+            className="rounded-full text-xs xl:text-sm font-normal px-3 py-0.5 h-6"
+          >
+            Dates
+          </Button>
+          <Button
+            onClick={handleCategoryClick}
+            id="hangouts"
+            variant={`${clickedCategory === "hangouts" ? "default" : "outline"}`}
+            className="rounded-full text-xs xl:text-sm font-normal px-3 py-0.5 h-6"
+          >
+            Hangouts
+          </Button>
+          <Button
+            onClick={handleCategoryClick}
+            id="shops"
+            variant={`${clickedCategory === "shops" ? "default" : "outline"}`}
+            className="rounded-full text-xs xl:text-sm font-normal px-3 py-0.5 h-6"
+          >
+            Shops
+          </Button>
+          <Button
+            onClick={handleCategoryClick}
+            id="adventures"
+            variant={`${clickedCategory === "adventures" ? "default" : "outline"}`}
+            className="rounded-full text-xs xl:text-sm font-normal px-3 py-0.5 h-6"
+          >
+            Adventures
+          </Button>
+          <Button
+            onClick={handleCategoryClick}
+            id="relaxations"
+            variant={`${clickedCategory === "relaxations" ? "default" : "outline"}`}
+            className="rounded-full text-xs xl:text-sm font-normal px-3 py-0.5 h-6"
+          >
+            Relaxations
+          </Button>
+          <Button
+            onClick={handleCategoryClick}
+            id="attractions"
+            variant={`${clickedCategory === "attractions" ? "default" : "outline"}`}
+            className="rounded-full text-xs xl:text-sm font-normal px-3 py-0.5 h-6"
+          >
+            Attractions
+          </Button>
+          <Button
+            onClick={handleCategoryClick}
+            id="celebrations"
+            variant={`${clickedCategory === "celebrations" ? "default" : "outline"}`}
+            className="rounded-full text-xs xl:text-sm font-normal px-3 py-0.5 h-6"
+          >
+            Celebrations
+          </Button>
+          <Button
+            onClick={handleCategoryClick}
+            id="others"
+            variant={`${clickedCategory === "others" ? "default" : "outline"}`}
+            className="rounded-full text-xs xl:text-sm font-normal px-3 py-0.5 h-6"
+          >
+            Others
+          </Button>
+        </div>
+        <div className="flex gap-0.5 items-center w-full justify-end">
+          <Button
+            onClick={handleSortClick}
+            id="newest"
+            variant="ghost"
+            className={`text-xs xl:text-sm font-normal px-2 py-0.5 h-6 ${clickedSort === "newest" ? "text-black" : "text-gray-400"}`}
+          >
+            Newest
+          </Button>
+          <Button
+            onClick={handleSortClick}
+            id="popular"
+            variant="ghost"
+            className={`text-xs xl:text-sm font-normal px-2 py-0.5 h-6 ${clickedSort === "popular" ? "text-black" : "text-gray-400"}`}
+          >
+            Most Popular
+          </Button>
+        </div>
+      </div>
+
       {data &&
         data.map((item: ListItem) => (
           <div key={item._id}>
