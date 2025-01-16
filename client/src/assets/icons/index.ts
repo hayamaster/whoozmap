4bca5c095d89faa1e69dd5fe84b531b40c5be49a
hayamaster <dnfxmfkdbzos@naver.com>
2025-01-16 01:22:57 -0500

    feat: add googleLoginIcon svg

--- client/src/assets/icons/index.ts
+++ client/src/assets/icons/index.ts
@@ -2,3 +2,4 @@ export { default as Logo } from "./logo.svg?react";
 export { default as SearchIcon } from "./searchIcon.svg?react";
 export { default as SaveIcon } from "./saveIcon.svg?react";
 export { default as CloseIcon } from "./closeIcon.svg?react";
+export { default as GoogleLoginIcon } from "./googleLoginIcon.svg?react";
