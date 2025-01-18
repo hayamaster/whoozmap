a04f0a45e73057d408a7c371954c7e069f702944
hayamaster <dnfxmfkdbzos@naver.com>
2025-01-18 16:32:18 -0500

    feat: create useGetUserDetails func

--- client/src/apis/hooks/index.ts
+++ client/src/apis/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useGetListItems } from "./useGetListItems";
 export { default as usePostRegister } from "./usePostRegister";
 export { default as usePostLogin } from "./usePostLogin";
+export { default as useGetUserDetails } from "./useGetUserDetails";
---
+++ client/src/apis/hooks/useGetUserDetails.ts
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../apiClient";
+
+const useGetUserDetails = () => {
+  const getUserDetails = async () => {
+    const response = await apiClient.get("/api/user-details");
+
+    return response.data.data;
+  };
+
+  const { data, refetch } = useQuery({
+    queryKey: ["userDetails"],
+    queryFn: () => getUserDetails(),
+  });
+
+  return { data, refetch };
+};
+
+export default useGetUserDetails;
