664750255f63c30bda8194fb75ceb2d195914c67
hayamaster <dnfxmfkdbzos@naver.com>
2025-01-19 03:36:16 -0500

    refactor: move appwrite project id to env file

--- client/src/appwrite/appwrite.ts
+++ client/src/appwrite/appwrite.ts
@@ -2,7 +2,7 @@ import { Client, Account, OAuthProvider } from "appwrite";
 
 const client = new Client()
   .setEndpoint("https://cloud.appwrite.io/v1") // The Appwrite API endpoint
-  .setProject("678c9cc90031aeb4e733"); // Your Appwrite project ID
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID); // Your Appwrite project ID
 
 const account = new Account(client);
 
