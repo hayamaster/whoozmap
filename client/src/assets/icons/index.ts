cc85239ca9d884f6ab5e20bd9a4af46ea86f4e25
hayamaster <dnfxmfkdbzos@naver.com>
2025-01-23 14:24:39 -0500

    feat: add hamburgerIcon

--- client/src/assets/icons/index.ts
+++ client/src/assets/icons/index.ts
@@ -3,3 +3,4 @@ export { default as SearchIcon } from "./searchIcon.svg?react";
 export { default as SaveIcon } from "./saveIcon.svg?react";
 export { default as CloseIcon } from "./closeIcon.svg?react";
 export { default as GoogleLoginIcon } from "./googleLoginIcon.svg?react";
+export { default as HamburgerIcon } from "./hamburgerIcon.svg?react";
