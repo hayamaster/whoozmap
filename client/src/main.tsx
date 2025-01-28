98110c3415dfbffbf70463ebc10f1dc77c655609
hayamaster <dnfxmfkdbzos@naver.com>
2025-01-28 13:12:12 -0500

    chore: delete react cookie

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { CookiesProvider } from "react-cookie";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <CookiesProvider>
-      <App />
-    </CookiesProvider>
+    <App />
   </StrictMode>
 );
