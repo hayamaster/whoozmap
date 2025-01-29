5fefa12c154652c3fcf7073bf3a09d0e332b8f72
hayamaster <dnfxmfkdbzos@naver.com>
2025-01-29 15:27:27 -0500

    feat: design detail for description in sidebar of CreateMapPage

--- client/src/pages/CreateMapPage.tsx
+++ client/src/pages/CreateMapPage.tsx
@@ -1,6 +1,6 @@
 import { Status, Wrapper } from "@googlemaps/react-wrapper";
 import { GoogleMap, CreateMapDetailsModal } from "@/components";
-import { SearchIcon } from "@/assets/icons";
+import { SearchIcon, DetailArrowIcon } from "@/assets/icons";
 import { useEffect, useState } from "react";
 
 const render = (status: Status) => {
@@ -18,6 +18,7 @@ const CreateMapPage = () => {
   const [search, setSearch] = useState("");
   const [showCreateMapDetailsModal, setShowCreateMapDetailsModal] =
     useState(false);
+  const [showDetailDescription, setShowDetailDescription] = useState(false);
 
   useEffect(() => {
     setShowCreateMapDetailsModal(true);
@@ -27,6 +28,10 @@ const CreateMapPage = () => {
     setShowCreateMapDetailsModal((prev) => !prev);
   };
 
+  const handleClickDetailDescription = () => {
+    setShowDetailDescription((prev) => !prev);
+  };
+
   return (
     <div className="w-screen h-screen flex flex-col">
       {showCreateMapDetailsModal && (
@@ -34,7 +39,7 @@ const CreateMapPage = () => {
       )}
       <div className="relative grid grid-rows-2 sm:grid-rows-none sm:grid-cols-2 lg:grid-cols-[500px,1fr] w-full h-full">
         <div className="order-last sm:order-first h-full w-full flex flex-col z-10 bg-white">
-          <div className="flex flex-col h-full w-full pt-7 sm:pt-10 px-4 sm:px-10 gap-7 sm:gap-10">
+          <div className="relative flex flex-col h-full w-full pt-7 sm:pt-10 px-4 sm:px-10 gap-7 sm:gap-10">
             <div className="flex fixed top-0 left-0 sm:static mx-4 mt-4 sm:mx-0 sm:mt-0 bg-white justify-center h-[50px] shrink-0 w-[calc(100%-32px)] sm:w-full px-5 border border-[#CCCCCC] rounded-full overflow-hidden items-center">
               <input
                 type="text"
@@ -55,7 +60,21 @@ const CreateMapPage = () => {
                   <p className="text-base leading-5 text-[#777777]">Category</p>
                 </div>
               </div>
-              <p>This is description</p>
+              <div className="flex w-full gap-1 items-center">
+                <p
+                  className={`w-full text-base leading-5 text-[#777777] ${showDetailDescription && "truncate"}`}
+                >
+                  Lorem ipsum dolor sit amet consectetur. Imperdiet
+                </p>
+                <i
+                  className="h-6 w-6 flex justify-center items-center cursor-pointer"
+                  onClick={handleClickDetailDescription}
+                >
+                  <DetailArrowIcon
+                    className={`w-6 h-6 transform transition-transform duration-300 ${showDetailDescription && "-rotate-180"}`}
+                  />
+                </i>
+              </div>
               <button
                 className="w-full h-[34px] rounded-2xl bg-[#EDEDED] font-semibold"
                 type="button"
@@ -65,11 +84,11 @@ const CreateMapPage = () => {
               </button>
             </div>
             <p className="leading-5 font-bold">0 Places Added</p>
-            <div className="w-full h-full text-base leading-5 md:text-lg lg:text-xl xl:text-2xl text-[#777777] font-bold sm:leading-7 flex flex-col justify-center items-center">
+            <div className="w-full h-full overflow-y-scroll text-base leading-5 md:text-lg lg:text-xl xl:text-2xl text-[#777777] font-bold sm:leading-7 flex flex-col justify-center items-center">
               <p>Add your favorite places and</p>
               <p>create own your map!</p>
             </div>
-            <div className="h-32 w-full py-7 sm:py-10">
+            <div className="flex h-fit w-full py-7 sm:py-10">
               <button className="w-full h-12 bg-[#EDEDED] rounded-2xl font-semibold leading-5">
                 Create
               </button>
