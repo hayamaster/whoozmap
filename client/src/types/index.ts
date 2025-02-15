948bdd1bdf924d1da460f8657e4fe61e10241b1d
hayamaster <dnfxmfkdbzos@naver.com>
2025-02-14 22:48:16 -0500

    refactor: create types file in frontend

---
+++ client/src/types/index.ts
@@ -0,0 +1,20 @@
+export type LatLng = {
+  lat: number;
+  lng: number;
+};
+
+export interface MapCreateMetaDataType {
+  title: string;
+  description: string;
+  thumbnailUrl: string;
+  categories: string[];
+}
+
+export interface GoogleMapsPlaceType {
+  name: string;
+  placeId: string;
+  lat: number;
+  lng: number;
+  icon: string;
+  location: string;
+}
