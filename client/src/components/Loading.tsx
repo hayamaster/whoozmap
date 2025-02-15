9b795499e1e6afcdfe459f13742ffde495ee6165
hayamaster <dnfxmfkdbzos@naver.com>
2025-02-14 21:54:55 -0500

    refactor: modify Loading component

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -6,7 +6,7 @@ interface LoadingProps {
 
 const Loading = ({ size = "sm" }: LoadingProps) => {
   return (
-    <div className="fixed top-0 left-0 w-screen h-dvh flex items-center justify-center bg-gray-700 bg-opacity-30 z-50">
+    <div className="absolute top-0 left-0 w-full h-dvh flex items-center justify-center bg-gray-700 bg-opacity-30 z-50">
       <Spinner size={size} className="bg-black dark:bg-white" />
     </div>
   );
